c11d27afab8e394df07f974dd523a8a473fd4ca3
Ricardo Odicio <ricardo.odicio@gmail.com>
2026-04-09 00:21:32 -0500

    02_bronze_reclamos.py

--- notebooks/02_bronze_reclamos.py
+++ notebooks/02_bronze_reclamos.py
@@ -1,7 +1,14 @@
+# Databricks notebook source
 from pyspark.sql import functions as F
 
 df_raw = spark.table("raw_reclamos")
 
-df_bronze = df_raw.withColumn("fecha_proceso_bronze", F.current_timestamp())
+df_bronze = (
+    df_raw
+    .withColumn("fecha_proceso_bronze", F.current_timestamp())
+)
 
 df_bronze.write.mode("overwrite").format("delta").saveAsTable("bronze_reclamos")
+
+display(df_bronze.limit(10))
+print(f"Registros cargados en bronze_reclamos: {df_bronze.count()}")
